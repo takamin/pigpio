94b92a71522270240375a887ad007b902e848c92
Koji Takami <vzg03566@gmail.com>
2015-05-24 06:47:34 +0000

    add pedsignal GPIO definition.

---
+++ pedsignal/pedsignal.c
@@ -0,0 +1,19 @@
+/*
+ * GPIO ports definition
+ *
+ * Signal for cars
+ *
+ * GPIO_2 - Blue
+ * GPIO_3 - Yellow
+ * GPIO_4 - Red
+ *
+ * Signal for pedestrian
+ *
+ * GPIO_14 - Walk
+ * GPIO_15 - Stop
+ *
+ * Message LED for pedestrian
+ *
+ * GPIO_17 - Push the button to abroad
+ * GPIO_18 - Wait till the signal turn to 'Walk'
+ */
